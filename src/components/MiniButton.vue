7d7f912226e92b787e82e76d7f3c20ce712c6299
Bayu Eko Saputro <bayu.eko@daunbiru.com>
2022-10-01 11:38:03 +0700

    fix audio autoplay 3

--- src/components/MiniButton.vue
+++ src/components/MiniButton.vue
@@ -13,10 +13,12 @@
     <source src="@/assets/audio/ICantSeemToLetYouGo.mp3" type="audio/mp3">
   </audio>
   <section class="fixed bottom-24 pl-2">
+    <!--
     <button class="button" @click="control">
       <i v-if="isPlayed" class="fa-solid fa-volume-high"></i>
       <i v-else class="fa-solid fa-volume-off"></i>
     </button>
+    -->
     <button @click="giftAction" class="button">
       <i class="fa-solid fa-gift"></i>
     </button>
@@ -31,7 +33,7 @@ import { useState } from '@/stores/state.js'
 const state = useState()
 
 const audioEl = ref(null)
-const isPlayed = ref(true)
+const isPlayed = ref(false)
 
 const isAudioPlay = computed(() => state.isAudioPlay)
 
