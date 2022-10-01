38f0117f24bac5f8cfa2e6de0d8f8c7388cf1053
Bayu Eko Saputro <bayu.eko@daunbiru.com>
2022-10-01 21:50:53 +0700

    Add font Aniyah

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,7 +1,8 @@
 <template>
   <section data-aos="fade-up" :class="props.textColor" class="w-full px-4 mb-6 text-center">
     <decoration class="fill-amber-400 mx-auto w-3/12 mb-3"></decoration>
-    <h1 class="satisfy-font text-4xl font-medium mb-5">{{ props.title }}</h1>
+    <!-- <h1 class="satisfy-font text-4xl font-medium mb-5">{{ props.title }}</h1> -->
+    <h1 class="Aniyah-font text-4xl font-medium mb-5">{{ props.title }}</h1>
     <p class="text-sm">{{ subtitle }}</p>
   </section>
 </template>
